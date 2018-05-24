3061aad513430ba252fc68f6455dddbd315e6e1b
YL <yangl@91wanyou.cn>
2018-05-24 09:35:25 +0800

    init

---
+++ CASkills/CASkills/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CASkills
+//
+//  Created by Long Yang on 2018/5/24.
+//  Copyright © 2018年 Long Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CASkills/CASkills/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CASkills
+//
+//  Created by Long Yang on 2018/5/24.
+//  Copyright © 2018年 Long Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
